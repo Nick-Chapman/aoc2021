8e81fad947167f3f604ec7250658ce284abd9add
Nick Chapman <nic@amadido.co.uk>
2021-12-09 18:42:40 +0000

    less is more

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,8 +1,6 @@
 module Day9 (main) where
 
 import Data.List (sort)
-import Data.Maybe (listToMaybe)
-import Data.Map (Map)
 import qualified Data.Map as Map
 import Misc (check,collate)
 import Par4 (Par,parse,separated,nl,digit,some)
@@ -24,56 +22,21 @@ gram = separated nl (some digit)
 
 type Setup = [[Height]]
 type Height = Int
-
-part1 :: Setup -> Int
-part1 hss = res
-  where
-    coordinated =
-      concat [ [ ((x,y),h) | (x,h) <- zip [0..] hs ] | (y,hs) <- zip [0..] hss ]
-
-    m :: Map Pos Height = Map.fromList coordinated
-
-    nextDoorHeights :: Pos -> [Height]
-    nextDoorHeights pos =
-      [ h | p <- nextDoor pos, h <- maybe [] single $ Map.lookup p m ]
-      where single x = [x]
-
-    lowPointRisks =
-      [ h+1
-      | (pos,h) <- coordinated
-      ,  all (h <) (nextDoorHeights pos)
-      ]
-
-    res = sum lowPointRisks
-
 type Pos = (Int,Int)
 
-nextDoor :: Pos -> [Pos]
-nextDoor (x,y) = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+part1 :: Setup -> Int
+part1 hss = sum [ h+1 | (pos,h) <- Map.toList m,  all (h <) (adjacentHeights pos)]
+  where
+    m = Map.fromList [ ((x,y),h) | (y,hs) <- zip [0..] hss, (x,h) <- zip [0..] hs ]
+    adjacentHeights pos = [ h | p <- adjacent pos, h <- maybe [] pure $ Map.lookup p m ]
 
 part2 :: Setup -> Int
-part2 hss =
-  product $ take 3 $ reverse $ sort [ length b | (_,b) <- basinsByLowPoint ]
-
+part2 hss = product $ take 3 $ reverse $ sort [ length b | (_,b) <- basinsByLowPoint ]
   where
-    coordinated =
-      concat [ [ ((x,y),h) | (x,h) <- zip [0..] hs ] | (y,hs) <- zip [0..] hss ]
+    basinsByLowPoint = collate [ (flow p h, p) | (p,h) <- Map.toList m, h < 9 ]
+    m = Map.fromList [ ((x,y),h) | (y,hs) <- zip [0..] hss, (x,h) <- zip [0..] hs ]
+    adjacentPH pos = [ (p,h) | p <- adjacent pos, h <- maybe [] pure $ Map.lookup p m ]
+    flow p h = case [ ph | ph@(_,h') <- adjacentPH p, h' < h ] of [] -> p; (p,h):_ -> flow p h
 
-    m :: Map Pos Height = Map.fromList coordinated
-
-    nextDoorHeights :: Pos -> [(Pos,Height)]
-    nextDoorHeights pos =
-      [ (p,h) | p <- nextDoor pos, h <- maybe [] pure $ Map.lookup p m ]
-
-    flow1 :: Pos -> Maybe Pos -- single step flow, or else we are low point
-    flow1 p = do
-      let myH = maybe (error "myH") id $ Map.lookup p m
-      listToMaybe [ p' | (p',h') <- nextDoorHeights p, h' < myH ]
-
-    flow :: Pos -> Pos -- final flow point. maybe self
-    flow p =
-      case flow1 p of
-        Nothing -> p
-        Just p' -> flow p'
-
-    basinsByLowPoint = collate [ (flow p, p) | (p,h) <- coordinated, h < 9 ]
+adjacent :: Pos -> [Pos]
+adjacent (x,y) = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
